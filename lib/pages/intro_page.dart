5bc053b4bc237be0be0bf5f45dda6e8d74598546
moritz <baarsmoritz70@gmail.com>
2025-04-01 18:35:03 +0200

    Fixing Map

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import "package:map_mates/pages/home_page.dart";
+import "package:map_mates/services/location_service.dart";
 import "package:flutter/material.dart";
 
 class IntroPage extends StatefulWidget {
@@ -20,37 +21,44 @@ class _IntroPageState extends State<IntroPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(height: 75),
-
-              // title
-              Text(
+              // Title
+              const Text(
                 "Just Do It",
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
               ),
-
               const SizedBox(height: 24),
-              // sub title
-              Text(
+              // Sub Title
+              const Text(
                 "Brand new Sneakers and custom Kicks made with premium Quality",
                 style: TextStyle(color: Colors.grey, fontSize: 16),
                 textAlign: TextAlign.center,
               ),
-
               const SizedBox(height: 48),
-
-              //start now button
+              // Start now button
               GestureDetector(
-                onTap:
-                    () => Navigator.push(
+                onTap: () async {
+                  // Standortberechtigung vor dem Navigieren anfordern
+                  bool permissionGranted = await LocationService.checkAndRequestLocationPermission();
+                  if (permissionGranted) {
+                    Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => HomePage()),
-                    ),
+                    );
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Standortberechtigung ist erforderlich, um fortzufahren.'),
+                      ),
+                    );
+                  }
+                },
                 child: Container(
                   decoration: BoxDecoration(
                     color: Colors.grey[900],
                     borderRadius: BorderRadius.circular(12),
                   ),
                   padding: const EdgeInsets.all(25),
-                  child: Center(
+                  child: const Center(
                     child: Text(
                       "Shop Now",
                       style: TextStyle(color: Colors.white),
